69a0acf79947973c6f9efd48d5e56672c7c6bbd0
Kush Mittal <kushi.mittal@gmail.com>
2015-11-07 20:29:21 +0000

    FIle can now read in images

--- Base.py
+++ Base.py
@@ -1,10 +1,13 @@
-
+from numpy import *
 
 def readTrainingLabels():
-	file = open("digitdata/traininglabels" , "r")
+	# Open The File
+	file = open("digitdata/traininglabels.txt" , "r")
 
+	# Count each digit in training set
 	list = [0,0,0,0,0,0,0,0,0,0]
 
+	# Total num of digits
 	counter = 0
 
 	for line in file:
@@ -13,13 +16,50 @@ def readTrainingLabels():
 
 	file.close()
 
-	return [float(x)/counter for x in list]
+	return list
+
+def readTrainingImages():
+	# Open the Training Images
+	file = open("digitdata/trainingimages.txt", "r")
+
+	# The List to store the images
+	list = []
+
+	curr_image = zeros((28,28))
+	i = 0
+
+
+	for line in file:
+		# Remove the \n
+		line = line.rstrip()
+
+		j = 0
+
+		for character in line:
+			if line[j] != ' ':
+				curr_image[(i)%28][j] = 1
+			j+=1
+
+		i+=1
+
+		if (i%28) == 0:
+			list.append(curr_image)
+			curr_image = zeros((28,28))
+
+	return list
+
 
 
 def main():
-	prior = readTrainingLabels()
+	#priorList = readTrainingLabels()
+	imagesList = readTrainingImages()
 
+	#print(priorList)
+	#print(imagesList)
+	file = open("digitdata/trainingimages_out.txt", "w")
 
+	#for item in imagesList:
+	#	print>>file, item
 
 if __name__ == '__main__':
 	main()
