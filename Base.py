27647d458b0e4a23a4e280e197d9a9d87122e9c0
Braydenturner <mailto:Braydenturner94@gmail.com>
2015-11-07 19:31:11 +0000

    Base.py edited online with Bitbucket

---
+++ Base.py
@@ -0,0 +1,14 @@
+def readTrainingLabels():
+	file = open("digitdata/traininglabels" , "r")
+
+	list = [0,0,0,0,0,0,0,0,0,0]
+
+	counter = 0
+
+	for line in file:
+		list[int(line.strip())]+=1
+		counter+=1
+
+	file.close()
+
+	print(list)
