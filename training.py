ecec02ed9704f51f1339d206c0a4cf5c87f0f119
Kush Mittal <kushi.mittal@gmail.com>
2015-11-14 18:19:44 +0000

    Improved Smoothing Algorithm for estimating likelihoods

--- training.py
+++ training.py
@@ -1,23 +1,17 @@
 from numpy import *
 
-def likelihood(list):
-	empirical_likelihood = zeros((28,28))
-	for x in list:
-		empirical_likelihood = add(empirical_likelihood, x)
-
-	return empirical_likelihood/len(list)
-
-
+"""
+This function computes the likelihood using
+Laplace smoothing for a number class object 
+based on the test data and the value of the 
+smoothing constant
+"""
 def smoothed_likelihood(list, k):	
 	empirical_likelihood = zeros((28,28))
 	for x in list:
 		empirical_likelihood = add(empirical_likelihood, x)
 
 	return (empirical_likelihood+k)/(len(list) + 2*k)
-
-
-def main():
-	print "Hello!"
 
 if __name__ == '__main__':
 	main()
