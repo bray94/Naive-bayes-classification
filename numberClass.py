78222a9e9defaf9bb556271ed4769d2fa67b854d
Kush Mittal <kushi.mittal@gmail.com>
2015-11-14 18:18:22 +0000

    Small changes to numberClass.py to increase functionality

--- numberClass.py
+++ numberClass.py
@@ -1,5 +1,19 @@
 from numpy import *
 
+"""
+This class represents a number along
+with the parameters that every class
+requires.
+classValue = The numeric value of the class
+prior = The prior probability of this class 
+        based on empirical frequencies
+empirical_likelihood = matrix of likelihoods
+        for every pixel location to be a 
+        forground pixel
+training_data = list of 2d arrays where each
+		2d array is the training image that
+		belongs to this class
+"""
 class numberClass(object):
 
 	def __init__(self, value):
@@ -7,6 +21,10 @@ class numberClass(object):
 		self.prior= .1
 		self.empirical_likelihood = zeros((28,28))
 		self.training_data = []
+		self.highestPosterior = -10000
+		self.lowestPosterior = 1000
+		self.highPostImage = zeros((28,28))
+		self.lowPostImage = zeros((28,28))
 
 	def addTrainingData(self, training_value):
 		self.training_data.append(training_value)
@@ -16,3 +34,5 @@ class numberClass(object):
 
 if __name__ == '__main__':
 	main()
+
+
